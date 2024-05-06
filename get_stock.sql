875de95b88dd7797ffa8a7c55e766b50fc8dd2eb
Lorcan Rae <lorcanrae@gmail.com>
2024-05-06 11:42:49 +0300

    update get_stock.sql

--- get_stock.sql
+++ get_stock.sql
@@ -1,5 +1,5 @@
 SELECT
   MODEL_ID
-  , STOCK
-  , DATE_CREATION
+  , CAST(STOCK AS INT64) AS STOCK
+  , CAST(DATE_CREATION AS DATE)
 FROM `data-analytics-bootcamp-363212.course14.circle_stock_clean`
