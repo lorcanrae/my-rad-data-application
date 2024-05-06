bf6a624a471e506f60c799232b66184b07d789aa
Lorcan Rae <lorcanrae@gmail.com>
2024-05-06 12:24:52 +0300

    Updated model_group.sql with calc for total val

--- model_group.sql
+++ model_group.sql
@@ -1,5 +1,6 @@
 SELECT
   MODEL_TYPE
-  , SUM(STOCK)
+  , SUM(STOCK) AS TOTAL_STOCK
+  , SUM(ROUND(PRICE * STOCK, 2)) AS TOTAL_INVENTORY_VALUE
 FROM `data-analytics-bootcamp-363212.course14.circle_stock_clean`
 GROUP BY MODEL_TYPE
